ac4639cfa1d3ca5c29188caf47c63072f8dc1775
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-06-22 00:56:02 +0530

    End recording on key press implemented

--- misc/record_audio.py
+++ misc/record_audio.py
@@ -1,7 +1,22 @@
-from sys import argv
+#!/usr/bin/env python
+
+import sys, select, os
+import termios
+import contextlib
 
 import wave
 import pyaudio
+
+@contextlib.contextmanager
+def raw_mode(file):
+    old_attrs = termios.tcgetattr(file.fileno())
+    new_attrs = old_attrs[:]
+    new_attrs[3] = new_attrs[3] & ~(termios.ECHO | termios.ICANON)
+    try:
+        termios.tcsetattr(file.fileno(), termios.TCSADRAIN, new_attrs)
+        yield
+    finally:
+        termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
 
 def record(OUTPUT_FILENAME):
 
@@ -19,29 +34,33 @@ def record(OUTPUT_FILENAME):
     stream = audio.open(format=_format, channels=_channels,
                         rate=_rate, input=True,
                         frames_per_buffer=_chunk)
-    print "STARTED. PRESS CTRL+C TO STOP RECORDING"
+    print "STARTED. PRESS ENTER KEY TO STOP RECORDING"
     frames = []
 
-    while 1:
-        try:
-            data = stream.read(_chunk)
-            frames.append(data)
-        except KeyboardInterrupt:
-            print ("STOPPING RECORDING")
-            # stop Recording
-            stream.stop_stream()
-            stream.close()
-            audio.terminate()
+    with raw_mode(sys.stdin):
+        while True:
+            if not (sys.stdin in select.select([sys.stdin], [], [], 0)[0]):
+                data = stream.read(_chunk)
+                frames.append(data)
+            else:
+                line = raw_input()
+                print ("STOPPING RECORDING")
+                # stop Recording
+                stream.stop_stream()
+                stream.close()
+                audio.terminate()
 
-            _wave_file = wave.open(OUTPUT_FILENAME, 'wb')
-            _wave_file.setnchannels(_channels)
-            _wave_file.setsampwidth(audio.get_sample_size(_format))
-            _wave_file.setframerate(_rate)
-            _wave_file.writeframes(b''.join(frames))
-            _wave_file.close()
+                _wave_file = wave.open(OUTPUT_FILENAME, 'wb')
+                _wave_file.setnchannels(_channels)
+                _wave_file.setsampwidth(audio.get_sample_size(_format))
+                _wave_file.setframerate(_rate)
+                _wave_file.writeframes(b''.join(frames))
+                _wave_file.close()
+                break
+
 
 if __name__ == '__main__':
     OUTPUT_FILENAME = 'demo.wav'
-    if len(argv) > 1:
-        OUTPUT_FILENAME = argv[1] + '.wav'
+    if len(sys.argv) > 1:
+        OUTPUT_FILENAME = sys.argv[1] + '.wav'
     record(OUTPUT_FILENAME)
