7f3bfe074521fce5ae446da54bf538967e46384c
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-06-22 07:48:55 +0530

    Integrated kws

--- misc/set_kws_threshold.py
+++ misc/set_kws_threshold.py
@@ -1,7 +1,12 @@
-import numpy as np
+"""sdff"""
 import os
+import numpy as np
+
+from pocketsphinx.pocketsphinx import *
+from sphinxbase.sphinxbase import *
 
 frequency_threshold = 3
+
 def analyse_file(path):
     with open(path) as f:
         content = f.readlines()
@@ -22,12 +27,50 @@ def analyse_file(path):
 #     print (test_case)
 #     record_audio()
     os.system('python record_audio.py test_case_audio')
+    kws_analysis(test_case)
+
+def kws_analysis(test_case):
+    analysis_result = []
+
+    modeldir = "/usr/local/share/pocketsphinx/model/"
+
+    # Create a decoder with certain model
+    config = Decoder.default_config()
+    config.set_string('-hmm', os.path.join(modeldir, 'en-us/en-us'))
+    config.set_string('-dict', 'voice_cmd.dic')
+    config.set_string('-kws', 'voice_cmd.kwlist')
+
+    stream = open(os.path.join("cmds7.wav"), "rb")
+
+    # Process audio chunk by chunk. On keyphrase detected perform action and restart search
+    decoder = Decoder(config)
+    decoder.start_utt()
+    while True:
+        buf = stream.read(1024)
+        if buf:
+            decoder.process_raw(buf, False, False)
+        else:
+            break
+        
+        if decoder.hyp() != None:
+            for seg in decoder.seg():
+                analysis_result.append([seg.word, seg.prob])
+            # Output: [('forward', -617, 63, 121)]
+            print ("Detected keyphrase, restarting search")
+            decoder.end_utt()
+            decoder.start_utt()
+    print (analysis_result)
+
+def process_threshold(analysis_result, test_case):
     
 
-# def record_audio():
+
+
+
     
     
 if __name__ == '__main__':
 #     if len(argv) > 1:
 #     FILE_NAME = argv[1]
     analyse_file("/home/pankaj/catkin_ws/src/pocketsphinx/demo/voice_cmd.dic")
+    # kws_analysis()
